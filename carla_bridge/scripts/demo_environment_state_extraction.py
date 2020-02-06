be4bba90e2e92320b3ab34e7777d0d1dc59fa6bb
MayankSingal <mayanksi@andrew.cmu.edu>
2020-02-06 09:37:51 -0500

    Creating branch

--- carla_bridge/scripts/demo_environment_state_extraction.py
+++ carla_bridge/scripts/demo_environment_state_extraction.py
@@ -16,13 +16,14 @@ import sys
 sys.path.insert(0, "/home/mayank/Mayank/MRSD_TeamD_Project")
 
 
-#import carla
+import carla
 import rospy
 import copy
 
-#sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
+sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
+from carla_handler import CarlaHandler
+sys.path.insert(0, '/opt/ros/kinetic/lib/python2.7/dist-packages')
 
-#from carla_handler import CarlaHandler
 from std_msgs.msg import String
 from grasp_path_planner.msg import LanePoint
 from grasp_path_planner.msg import LanePoint
@@ -35,18 +36,6 @@ from grasp_path_planner.msg import PathPlan
 
 #####################################################################################
 
-# Spawn some vehicles in the environment using spawn_vehicles.py.
-#subprocess.Popen(["python", "spawn_npc.py", "-n" , "70"])
-
-# Start Client. Make sure Carla server is running before starting.
-
-#client = carla.Client('localhost', 2000)
-#client.set_timeout(2.0)
-
-# Create a CarlaHandler object. CarlaHandler provides some cutom built APIs for the Carla Server.
-
-#carla_handler = CarlaHandler(client)
-
 NODE_NAME = 'carla_bridge'
 SIM_TOPIC_NAME = 'environment_state'
 PATH_PLAN_TOPIC_NAME = 'path_plan'
@@ -55,6 +44,38 @@ class CarlaManager:
 	def __init__(self):
 		self.env_pub = None
 		self.path_sub = None
+
+		## CARLA Deps ##
+		self.client = None
+		self.carla_handler = None
+
+	def getVehicleState(self, actor):
+		vehicle = VehicleState()
+		vehicle.vehicle_location.x = actor.get_transform().location.x
+		vehicle.vehicle_location.y = actor.get_transform().location.y
+		vehicle.vehicle_location.theta = actor.get_transform().rotation.yaw #TODO : This has roll, pith and yaw. Need 2 more variables?
+		vehicle.vehicle_speed = 3 # TODO : This has x, y, and z components. Need 2 more variables. 
+		vehicle.length = 5
+		vehicle.width = 2
+
+		return vehicle
+
+	def getLanePoints(self, waypoints):
+
+		lane_cur = Lane()
+		lane_points = []
+		for waypoint in waypoints:
+			lane_point = LanePoint()
+			lane_point.pose.y = waypoint.transform.location.y
+			lane_point.pose.x = waypoint.transform.location.x
+			lane_point.pose.theta = waypoint.transform.rotation.yaw # TODO : this has roll, pitch and yaw
+			lane_point.width = 3 # TODO
+			lane_points.append(lane_point)
+		lane_cur.lane = lane_points
+
+		return lane_cur
+
+
 
 	def pathCallback(self, data):
 		# sample path load code ToDo: Delete
@@ -134,49 +155,68 @@ class CarlaManager:
 		# initlize subscriber
 		self.path_sub = rospy.Subscriber(PATH_PLAN_TOPIC_NAME, PathPlan, self.pathCallback)
 
-		# ToDo: initialize anything relating to CARLA
+		################################## Initialize environment in CARLA #################################################
+		##
+		##	1. Spawn n = 70 actors in the map.
+		##	2. Spawn ego vehicle.
+		##
+		####
 
+		# Spawn some vehicles in the environment using spawn_vehicles.py, if environment doesn't have vehicles already.
+		# subprocess.Popen(["python", "/home/mayank/Mayank/GRASP_ws/src/MRSD_TeamD_Project/carla_bridge/scripts", "-n" , "70"])
+
+		# Start Client. Make sure Carla server is running before starting.
+
+		client = carla.Client('localhost', 2000)
+		client.set_timeout(2.0)
+		print("Connection to CARLA server established!")
+
+		# Create a CarlaHandler object. CarlaHandler provides some cutom built APIs for the Carla Server.
+
+		self.carla_handler = CarlaHandler(client)
+ 
+		# Spawn ego vehicle on road 
+		filtered_waypoints = self.carla_handler.filter_waypoints(self.carla_handler.get_waypoints(), road_id=12)
+		spawn_point = filtered_waypoints[100].transform # Select random point from filtered waypoint list #TODO Initialization Scheme Design
+		spawn_point.location.z = spawn_point.location.z + 2 # To avoid collision during spawn
+		ego_vehicle, ego_vehicle_ID = self.carla_handler.spawn_vehicle(spawn_point=spawn_point)
+
+		state_information = self.carla_handler.get_state_information(ego_vehicle)
+		current_lane_waypoints, left_lane_waypoints, right_lane_waypoints, front_vehicle, rear_vehicle, actors_in_current_lane, actors_in_left_lane, actors_in_right_lane = state_information
+
+		
+		##############################################################################################################
 		# publish the first frame ToDo: change it to an actual one
 		time.sleep(2)
 		rate = rospy.Rate(10000)
 		rate.sleep()#ToDo: Delete this line	
-		lane_cur = Lane()
-		lane_points = []
-		for i in range(100):
-			lane_point = LanePoint()
-			lane_point.pose.y = 0
-			lane_point.pose.x = i/10.
-			lane_point.pose.theta = 0
-			lane_point.width = 3
-			lane_points.append(lane_point)
-		lane_cur.lane = lane_points
-		# next Lane
-		lane_next = Lane()
-		lane_points_next = []
-		for i in range(100):
-			lane_point = LanePoint()
-			lane_point.pose.y = 3
-			lane_point.pose.x = i/10.
-			lane_point.pose.theta = 0
-			lane_point.width = 3
-			lane_points_next.append(lane_point)
-		lane_next.lane = lane_points_next
-	
-		# current vehicle	
-		vehicle = VehicleState()
-		vehicle.vehicle_location.x = 2
-		vehicle.vehicle_location.y = 0
-		vehicle.vehicle_location.theta = 0
-		vehicle.vehicle_speed = 10
-		vehicle.length = 5
-		vehicle.width = 2
 
+		# Current Lane
+		lane_cur = self.getLanePoints(current_lane_waypoints)
+		
+		# Left Lane
+		lane_left = self.getLanePoints(left_lane_waypoints)
+		
+		# Right Lane
+		lane_right = self.getLanePoints(right_lane_waypoints)
+		
+
+		# TODO : Can wrap this as a function member of the class. //Done  
+		# Ego vehicle	
+		vehicle_ego = self.getVehicleState(ego_vehicle);
+		
+		# Front vehicle	
+		vehicle_front = self.getVehicleState(front_vehicle)
+		
+		# Rear vehicle	
+		vehicle_rear = self.getVehicleState(rear_vehicle)
+	
 		# sample enviroment state
 		env_state = EnvironmentState()
-		env_state.cur_vehicle_state = copy.copy(vehicle)
-		env_state.front_vehicle_state = copy.copy(vehicle)
-		env_state.back_vehicle_state = copy.copy(vehicle)
-		env_state.adjacent_lane_vehicles = [copy.copy(vehicle), copy.copy(vehicle)]
+		env_state.cur_vehicle_state = copy.copy(vehicle_ego)
+		env_state.front_vehicle_state = copy.copy(vehicle_front)
+		env_state.back_vehicle_state = copy.copy(vehicle_rear)
+		env_state.adjacent_lane_vehicles = [copy.copy(self.getVehicleState(actor)) for actor in actors_in_left_lane] #TODO : Only considering left lane for now. Need to make this more general 
 		env_state.max_num_vehicles = 2
 		env_state.speed_limit = 40
 		self.env_pub.publish(env_state)
---
+++ carla_bridge/scripts/utils.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+""" Some useful functions. """
+
+__author__ = "Mayank Singal"
+__maintainer__ = "Mayank Singal"
+__email__ = "mayanksi@andrew.cmu.edu"
+__version__ = "0.1"
+
+import carla
+import numpy as np
+
+def get_matrix(transform):
+    """
+    Creates matrix from carla transform.
+    """
+
+    rotation = transform.rotation
+    location = transform.location
+    c_y = np.cos(np.radians(rotation.yaw))
+    s_y = np.sin(np.radians(rotation.yaw))
+    c_r = np.cos(np.radians(rotation.roll))
+    s_r = np.sin(np.radians(rotation.roll))
+    c_p = np.cos(np.radians(rotation.pitch))
+    s_p = np.sin(np.radians(rotation.pitch))
+    matrix = np.matrix(np.identity(4))
+    matrix[0, 3] = location.x
+    matrix[1, 3] = location.y
+    matrix[2, 3] = location.z
+    matrix[0, 0] = c_p * c_y
+    matrix[0, 1] = c_y * s_p * s_r - s_y * c_r
+    matrix[0, 2] = -c_y * s_p * c_r - s_y * s_r
+    matrix[1, 0] = s_y * c_p
+    matrix[1, 1] = s_y * s_p * s_r + c_y * c_r
+    matrix[1, 2] = -s_y * s_p * c_r + c_y * s_r
+    matrix[2, 0] = s_p
+    matrix[2, 1] = -c_p * s_r
+    matrix[2, 2] = c_p * c_r
+    return matrix
+
+
+
+def create_bb_points(vehicle):
+    """
+    Returns 3D bounding box for a vehicle.
+    """
+
+    cords = np.zeros((8, 4))
+    extent = vehicle.bounding_box.extent
+    cords[0, :] = np.array([extent.x, extent.y, -extent.z, 1])
+    cords[1, :] = np.array([-extent.x, extent.y, -extent.z, 1])
+    cords[2, :] = np.array([-extent.x, -extent.y, -extent.z, 1])
+    cords[3, :] = np.array([extent.x, -extent.y, -extent.z, 1])
+    cords[4, :] = np.array([extent.x, extent.y, extent.z, 1])
+    cords[5, :] = np.array([-extent.x, extent.y, extent.z, 1])
+    cords[6, :] = np.array([-extent.x, -extent.y, extent.z, 1])
+    cords[7, :] = np.array([extent.x, -extent.y, extent.z, 1])
+    return cords
