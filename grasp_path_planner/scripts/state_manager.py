d5393d20c958661266e6e618143424efe951c175
askarthikkumar@gmail.com <askarthikkumar@gmail.com>
2020-10-07 22:40:15 -0400

    Added lane following state creation

--- grasp_path_planner/scripts/state_manager.py
+++ grasp_path_planner/scripts/state_manager.py
@@ -25,7 +25,7 @@ class StateManager:
         Create a dictionary of states. They keys are "adjacent", "front", "back", "pedestrians", "lane_distance".
         Each value for the actor types is a numpy array of the size (n X 6), where is the number of actors of that type
         """
-        laneChangeState = {}
+        lane_change_state = {}
         ego_vehicle_state = [0, 0, 0]
         # extract all vehicles from adjacent lanes
         # TODO: Should I choose vehicles only from the lane to which we are shifting or all adjacent lanes?
@@ -35,7 +35,7 @@ class StateManager:
                 vehicle_state = [vehicle.location_frenet.x, vehicle.location_frenet.y, vehicle.location_frenet.theta]
                 adj_lane_vehicles_concatenated.append(ego_vehicle_state + vehicle_state)
 
-        laneChangeState["adjacent"] = np.array(adj_lane_vehicles_concatenated)
+        lane_change_state["adjacent"] = np.array(adj_lane_vehicles_concatenated)
         # extract front vehicle and back vehicle current lane
         current_lane = CurrentLane(env_desc.current_lane)
         front_vehicle = current_lane.vehicleInFront()
@@ -49,8 +49,8 @@ class StateManager:
                               back_vehicle.location_frenet.y,
                               back_vehicle.location_frenet.theta]
 
-        laneChangeState["front"] = np.array([ego_vehicle_state + front_vehicle_state])
-        laneChangeState["back"] = np.array([ego_vehicle_state + back_vehicle_state])
+        lane_change_state["front"] = np.array([ego_vehicle_state + front_vehicle_state])
+        lane_change_state["back"] = np.array([ego_vehicle_state + back_vehicle_state])
         # TODO: DO I add all the pedestrians which are relevant ? Maybe too much information
         pedestrian_states_concatenated = []
         for pedestrian in env_desc.pedestrians:
@@ -58,13 +58,38 @@ class StateManager:
                                 pedestrian.location_frenet.y,
                                 pedestrian.location_frenet.theta]
             pedestrian_states_concatenated.append(ego_vehicle_state + pedestrian_state)
-        laneChangeState["pedestrians"] = np.array(pedestrian_states_concatenated)
+        lane_change_state["pedestrians"] = np.array(pedestrian_states_concatenated)
         # Lane Distance
         # TODO: Identify which lane to consider for lane distance
-        laneChangeState["lane_distance"] = 0
+        lane_change_state["lane_distance"] = 0
 
-    def createLaneFollowingState(self):
-        pass
+    def createLaneFollowingState(self, env_desc):
+        lane_following_state = {}
+        ego_vehicle_state = [0, 0, 0]
+
+        # extract front vehicle and back vehicle current lane
+        current_lane = CurrentLane(env_desc.current_lane)
+        front_vehicle = current_lane.vehicleInFront()
+        back_vehicle = current_lane.vehicleBehind()
+
+        front_vehicle_state = [front_vehicle.location_frenet.x,
+                               front_vehicle.location_frenet.y,
+                               front_vehicle.location_frenet.theta]
+
+        back_vehicle_state = [back_vehicle.location_frenet.x,
+                              back_vehicle.location_frenet.y,
+                              back_vehicle.location_frenet.theta]
+
+        lane_following_state["front"] = np.array([ego_vehicle_state + front_vehicle_state])
+        lane_following_state["back"] = np.array([ego_vehicle_state + back_vehicle_state])
+        # TODO: DO I add all the pedestrians which are relevant ? Maybe too much information
+        pedestrian_states_concatenated = []
+        for pedestrian in env_desc.pedestrians:
+            pedestrian_state = [pedestrian.location_frenet.x,
+                                pedestrian.location_frenet.y,
+                                pedestrian.location_frenet.theta]
+            pedestrian_states_concatenated.append(ego_vehicle_state + pedestrian_state)
+        lane_following_state["pedestrians"] = np.array(pedestrian_states_concatenated)
 
     def createPedestrianState(self, env_desc):
         """
@@ -73,11 +98,10 @@ class StateManager:
         :param env_desc: (EnvironmentState) a ROS message describing the environment
         :param local: a bool flag describing whether the local or absolute coordinates must be used.
         """
-        env_state = {}
         # choose between local and absolute coordinate system
         # the state embedding contains the vehicle and the pedestrian
         # TODO: Need find the closest pedestrian? Do we even consider this case?
-        return env_state
+        raise NotImplementedError()
 
     def embedState(self, env_desc: EnvDesc, scenario: Scenario, local=False) -> np.ndarray:
         """
