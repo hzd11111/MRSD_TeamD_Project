9a3bbd11d9d805f51e8138d1cc12804f5ba1e27e
askarthikkumar@gmail.com <askarthikkumar@gmail.com>
2020-10-07 22:36:59 -0400

    added functionality for creating lane change state. removed pedestrian state

--- grasp_path_planner/scripts/state_manager.py
+++ grasp_path_planner/scripts/state_manager.py
@@ -1,67 +1,9 @@
 import numpy as np
 
 # ROS Packages
-from grasp_path_planner.msg import EnvironmentState
-from grasp_path_planner.msg import VehicleState
+from carla_utils.msg import EnvDesc, CurrentLane
 # other packages
 from settings import Scenario
-
-
-# TODO: Figure out if exactly how the heading works
-def convertToLocal(cur_vehicle: VehicleState, adj_vehicle: VehicleState):
-    """
-    Converts the adj_vehicle position into cur_vehicle position
-    Expects VehicleState objects and Euclidean Coordinate Frame
-
-    Args
-    :param cur_vehicle: (VehicleState) the ego vehicle state
-    :param adj_vehicle: (VehicleState) the target vehicle state
-
-    Returns
-    the adj_vehicle pose in terms of the cur_vehicle
-    """
-    # create a place holder vehicle object to represent the resultant relative pose
-    result_state = VehicleState()
-
-    # extract the pose and velocities of the adj_vehicle
-    x = adj_vehicle.vehicle_location.x
-    y = adj_vehicle.vehicle_location.y
-    theta = adj_vehicle.vehicle_location.theta
-    speed = adj_vehicle.vehicle_speed
-
-    # heading is in terms of the x-axis so vx is cos component
-    # vx = speed * np.cos(theta)
-    # vy = speed * np.sin(theta)
-
-    # get current_vehicle_speeds
-    # cvx = cur_vehicle.vehicle_speed * np.cos(cur_vehicle.vehicle_location.theta)
-    # cvy = cur_vehicle.vehicle_speed * np.sin(cur_vehicle.vehicle_location.theta)
-
-    # make homogeneous transform matrix to transform global coordinate into the frame
-    # of the ego vehicle state. Calculate as a product of two transformations. Multiply right.
-    H_Rot = np.eye(3)
-    H_Rot[-1, -1] = 1
-    H_Rot[0, -1] = 0
-    H_Rot[1, -1] = 0
-    H_Rot[0, 0] = np.cos(cur_vehicle.vehicle_location.theta)
-    H_Rot[0, 1] = np.sin(cur_vehicle.vehicle_location.theta)
-    H_Rot[1, 0] = -np.sin(cur_vehicle.vehicle_location.theta)
-    H_Rot[1, 1] = np.cos(cur_vehicle.vehicle_location.theta)
-    H_trans = np.eye(3)
-    H_trans[0, -1] = -cur_vehicle.vehicle_location.x
-    H_trans[1, -1] = -cur_vehicle.vehicle_location.y
-    H = np.matmul(H_Rot, H_trans)
-    # calculate and set relative position
-    res = np.matmul(H, np.array([x, y, 1]).reshape(3, 1))
-    result_state.vehicle_location.x = res[0, 0]
-    result_state.vehicle_location.y = res[1, 0]
-    # calculate and set relative orientation
-    result_state.vehicle_location.theta = theta - cur_vehicle.vehicle_location.theta
-    # calculate and set relative speed
-    # res_vel = np.array([vx - cvx, vy - cvy])
-    result_state.vehicle_speed = speed  # np.linalg.norm(res_vel)
-    # time.sleep(5)
-    return result_state
 
 
 class StateManager:
@@ -71,108 +13,84 @@ class StateManager:
     def __init__(self, event):
         self.event = event
 
-    def append_vehicle_state(self, env_state, vehicle_state):
+    def appendVehicleState(self, env_state, vehicle_state):
         env_state.append(vehicle_state.vehicle_location.x)
         env_state.append(vehicle_state.vehicle_location.y)
         env_state.append(vehicle_state.vehicle_location.theta)
         env_state.append(vehicle_state.vehicle_speed)
         return
 
-    def createLaneChangeState(self, env_desc, local):
-        pass
+    def createLaneChangeState(self, env_desc):
+        """
+        Create a dictionary of states. They keys are "adjacent", "front", "back", "pedestrians", "lane_distance".
+        Each value for the actor types is a numpy array of the size (n X 6), where is the number of actors of that type
+        """
+        laneChangeState = {}
+        ego_vehicle_state = [0, 0, 0]
+        # extract all vehicles from adjacent lanes
+        # TODO: Should I choose vehicles only from the lane to which we are shifting or all adjacent lanes?
+        adj_lane_vehicles_concatenated = []
+        for lane in env_desc.adjacent_lanes:
+            for vehicle in lane:
+                vehicle_state = [vehicle.location_frenet.x, vehicle.location_frenet.y, vehicle.location_frenet.theta]
+                adj_lane_vehicles_concatenated.append(ego_vehicle_state + vehicle_state)
+
+        laneChangeState["adjacent"] = np.array(adj_lane_vehicles_concatenated)
+        # extract front vehicle and back vehicle current lane
+        current_lane = CurrentLane(env_desc.current_lane)
+        front_vehicle = current_lane.vehicleInFront()
+        back_vehicle = current_lane.vehicleBehind()
+
+        front_vehicle_state = [front_vehicle.location_frenet.x,
+                               front_vehicle.location_frenet.y,
+                               front_vehicle.location_frenet.theta]
+
+        back_vehicle_state = [back_vehicle.location_frenet.x,
+                              back_vehicle.location_frenet.y,
+                              back_vehicle.location_frenet.theta]
+
+        laneChangeState["front"] = np.array([ego_vehicle_state + front_vehicle_state])
+        laneChangeState["back"] = np.array([ego_vehicle_state + back_vehicle_state])
+        # TODO: DO I add all the pedestrians which are relevant ? Maybe too much information
+        pedestrian_states_concatenated = []
+        for pedestrian in env_desc.pedestrians:
+            pedestrian_state = [pedestrian.location_frenet.x,
+                                pedestrian.location_frenet.y,
+                                pedestrian.location_frenet.theta]
+            pedestrian_states_concatenated.append(ego_vehicle_state + pedestrian_state)
+        laneChangeState["pedestrians"] = np.array(pedestrian_states_concatenated)
+        # Lane Distance
+        # TODO: Identify which lane to consider for lane distance
+        laneChangeState["lane_distance"] = 0
 
     def createLaneFollowingState(self):
         pass
 
-    def createPedestrianState(self, env_desc, local):
+    def createPedestrianState(self, env_desc):
         """
         Create a pedestrian avoidance event state
         Args:
         :param env_desc: (EnvironmentState) a ROS message describing the environment
         :param local: a bool flag describing whether the local or absolute coordinates must be used.
         """
-        env_state = []
+        env_state = {}
         # choose between local and absolute coordinate system
         # the state embedding contains the vehicle and the pedestrian
-        if not local:
-            self.append_vehicle_state(env_state, env_desc.cur_vehicle_state)
-            ped_vehicle = VehicleState()
-            ped_vehicle.vehicle_location = env_desc.nearest_pedestrian.pedestrian_location
-            ped_vehicle.pedestrian_speed = env_desc.nearest_pedestrian.pedestrian_speed
-            self.append_vehicle_state(env_state, ped_vehicle)
-        else:
-            cur_vehicle_state = VehicleState()
-            cur_vehicle_state.vehicle_location.x = 0
-            cur_vehicle_state.vehicle_location.y = 0
-            cur_vehicle_state.vehicle_location.theta = 0
-            cur_vehicle_state.vehicle_speed = env_desc.cur_vehicle_state.vehicle_speed
-            self.append_vehicle_state(env_state, cur_vehicle_state)
-            ped_vehicle = VehicleState()
-            ped_vehicle.vehicle_location = env_desc.nearest_pedestrian.pedestrian_location
-            ped_vehicle.vehicle_speed = env_desc.nearest_pedestrian.pedestrian_speed
-            converted_state = convertToLocal(env_desc.cur_vehicle_state, ped_vehicle)
-            self.append_vehicle_state(env_state, converted_state)
+        # TODO: Need find the closest pedestrian? Do we even consider this case?
         return env_state
 
-    def embedState(self, env_desc: EnvironmentState, scenario: Scenario, local=False) -> np.ndarray:
+    def embedState(self, env_desc: EnvDesc, scenario: Scenario, local=False) -> np.ndarray:
         """
         Create a state embedding vector from message recieved
         Args:
-        :param env_des: (EnvironmentState) a ROS message describing the environment
+        :param env_desc: (EnvironmentState) a ROS message describing the environment
         """
-        # TODO: Convert the embedding to Frenet coordinate system
         # based on scenario create the embedding accordingly
         if self.event == Scenario.LANE_CHANGE:
-            # initialize count of vehicles. We will add 5 vehicles
-            # The lane change scenario contains the current vehicle and 5 adjacent vehicles in the state
-            count_of_vehicles = 0
-            env_state = []
-
-            # if local flag is not set then use absolute coordinates
-            if not local:
-                self.append_vehicle_state(env_state, env_desc.cur_vehicle_state)
-                # TODO: Currently not adding vehicles in the front and back. Might need to add these later.
-                # self.append_vehicle_state(env_state, env_desc.back_vehicle_state)
-                # self.append_vehicle_state(env_state, env_desc.front_vehicle_state)
-                # count += 2
-                # add vehicles in the adjacent lane
-                for _, vehicle in enumerate(env_desc.adjacent_lane_vehicles):
-                    if count_of_vehicles < 5:
-                        self.append_vehicle_state(env_state, vehicle)
-                    else:
-                        break
-                    count_of_vehicles += 1
-            else:
-                # use relative coordinates. Current vehicle is origin
-                cur_vehicle_state = VehicleState()
-                cur_vehicle_state.vehicle_location.x = 0
-                cur_vehicle_state.vehicle_location.y = 0
-                cur_vehicle_state.vehicle_location.theta = 0
-                # Still choose absolute current_vehicle speed wrt global frame
-                cur_vehicle_state.vehicle_speed = env_desc.cur_vehicle_state.vehicle_speed
-                self.append_vehicle_state(env_state, cur_vehicle_state)
-
-                # TODO: Currently not adding vehicles in the front and back. Might need to add these later.
-                # add vehicles in the adjacent lane
-                for _, vehicle in enumerate(env_desc.adjacent_lane_vehicles):
-                    converted_state = convertToLocal(env_desc.cur_vehicle_state, vehicle)
-                    if count_of_vehicles < 5:
-                        self.append_vehicle_state(env_state, converted_state)
-                    else:
-                        break
-                    count_of_vehicles += 1
-
-            # add dummy vehicles into the state if not reached 5
-            dummy = VehicleState()
-            dummy.vehicle_location.x = 100
-            dummy.vehicle_location.y = 100
-            dummy.vehicle_location.theta = 0
-            dummy.vehicle_speed = 0
-            while count_of_vehicles < 5:
-                self.append_vehicle_state(env_state, dummy)
-                count_of_vehicles += 1
-            return env_state
-
+            return self.createLaneChangeState(env_desc)
+        elif self.event == Scenario.LANE_FOLLOWING:
+            return self.createLaneFollowingState(env_desc)
         elif self.event == Scenario.PEDESTRIAN:
-            # initialize an empty list for the state
-            return self.createPedestrianState(env_desc, local)
+            return self.createPedestrianState(env_desc)
+        else:
+            return {}
