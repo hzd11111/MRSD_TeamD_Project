8705d2c6e02e15c9daaed07d300d62df5571efd2
Alvin Huang <alvin121212@live.cn>
2020-10-09 14:28:47 -0400

    fixed path_planner.py

--- grasp_path_planner/scripts/path_planner.py
+++ grasp_path_planner/scripts/path_planner.py
@@ -2,16 +2,6 @@
 import math
 import rospy
 import numpy as np
-from geometry_msgs.msg import Pose2D
-from std_msgs.msg import String
-from grasp_path_planner.msg import LanePoint
-from grasp_path_planner.msg import Lane
-from grasp_path_planner.msg import VehicleState
-from grasp_path_planner.msg import RewardInfo
-from grasp_path_planner.msg import EnvironmentState
-from grasp_path_planner.msg import RLCommand
-from grasp_path_planner.msg import Pedestrian
-from grasp_path_planner.msg import PathPlan
 from grasp_path_planner.srv import SimService, SimServiceResponse, SimServiceRequest
 from trajectory_generator import TrajGenerator
 # other packages
@@ -40,87 +30,6 @@ else:
         'decelerate_amt' : 30,\
         'min_speed' : 0
     }
-    
-
-class VecTemp:
-    def __init__(self, x=0, y=0):
-        self.x = x
-        self.y = y
-
-    def norm(self):
-        return math.sqrt(self.x ** 2 + self.y ** 2)
-
-    def add(self, other):
-        return VecTemp(self.x + other.x, self.y + other.y)
-
-    def sub(self, other):
-        return VecTemp(self.x - other.x, self.y - other.y)
-
-    def dot(self, other):
-        upper = self.x * other.x + self.y * other.y
-        lower = self.norm() * other.norm()
-        if lower <= 0.00001:
-            return 1
-        return upper / lower
-
-
-class PoseTemp:
-    def __init__(self, ros_pose=False):
-        if ros_pose:
-            self.x = ros_pose.x
-            self.y = ros_pose.y
-            self.theta = self.wrapToPi(ros_pose.theta)
-        else:
-            self.x = 0
-            self.y = 0
-            self.theta = 0
-
-    def wrapToPi(self, theta):
-        return (theta + math.pi) % (2. * math.pi) - math.pi
-
-    def distance(self, pose):
-        return math.sqrt((self.x - pose.x) ** 2. + (self.y - pose.y) ** 2.)
-
-    def add(self, pose):
-        new_pose = PoseTemp()
-        new_pose.x = self.x + pose.x * math.cos(self.theta) - pose.y * math.sin(self.theta)
-        new_pose.y = self.y + pose.x * math.sin(self.theta) + pose.y * math.cos(self.theta)
-        new_pose.theta = self.wrapToPi(self.theta + pose.theta)
-        return new_pose
-
-    def vecTo(self, pose):
-        new_vec = VecTemp()
-        new_vec.x = pose.x - self.x
-        new_vec.y = pose.y - self.y
-        return new_vec
-
-    def vecFromTheta(self):
-        return VecTemp(math.cos(self.theta), math.sin(self.theta))
-
-    def isInfrontOf(self, pose):
-        diff_vec = pose.vecTo(self)
-        other_vec = pose.vecFromTheta()
-        return diff_vec.dot(other_vec) > 0
-
-    def scalarMultiply(self, scalar):
-        new_pose = PoseTemp()
-        new_pose.x = self.x * scalar
-        new_pose.y = self.y * scalar
-        new_pose.theta = self.theta * scalar
-
-
-class PoseSpeedTemp(PoseTemp):
-    def __init__(self):
-        PoseTemp.__init__(self)
-        self.speed = 0
-
-    def addToPose(self, pose):
-        new_pose = PoseSpeedTemp()
-        new_pose.x = pose.x + self.x * math.cos(pose.theta) - self.y * math.sin(pose.theta)
-        new_pose.y = pose.y + self.x * math.sin(pose.theta) + self.y * math.cos(pose.theta)
-        new_pose.theta = self.wrapToPi(self.theta + pose.theta)
-        new_pose.speed = self.speed
-        return new_pose
 
 class PathPlannerManager:
     def __init__(self):
