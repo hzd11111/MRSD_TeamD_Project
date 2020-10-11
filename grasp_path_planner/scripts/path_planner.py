851e545f419c21cb7dd684f4866c3ba8cd600c93
Alvin Huang <alvin121212@live.cn>
2020-10-11 01:12:01 -0400

    removed hard coded actoin

--- grasp_path_planner/scripts/path_planner.py
+++ grasp_path_planner/scripts/path_planner.py
@@ -48,8 +48,6 @@ class PathPlannerManager:
         self.sim_service_interface = rospy.ServiceProxy(SIM_SERVICE_NAME, SimService)
 
     def performAction(self, action):
-        action = RLDecision.SWITCH_LANE_LEFT
-
         path_plan = self.traj_generator.trajPlan(action, self.prev_env_desc)
 
         req = SimServiceRequest()
