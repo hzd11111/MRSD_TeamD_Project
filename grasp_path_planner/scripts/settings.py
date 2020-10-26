270560d8864d6d6f95c9458e1df4fd00fb05252b
askarthikkumar@gmail.com <askarthikkumar@gmail.com>
2020-10-26 18:46:22 -0400

    added is_misc vehicles into state and nets

--- grasp_path_planner/scripts/settings.py
+++ grasp_path_planner/scripts/settings.py
@@ -12,7 +12,7 @@ class Mode(Enum):
 
 ############ Mode and Model Selection ##############################
 CURRENT_SCENARIO = Scenario.LEFT_TURN
-CURRENT_MODE = Mode.TEST
+CURRENT_MODE = Mode.TRAIN
 
 if CURRENT_SCENARIO == Scenario.LANE_FOLLOWING:
     MODEL_SAVE_PATH = dir_path + "/Models/DQN_Lane_Following"
